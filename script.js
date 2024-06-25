5e8a8a847314b641a11c1d9017497c4a85cd4c4b
edwinkim16 <edwinkimathi226@gmail.com>
2024-06-25 10:55:12 +0300

    update overall function

--- script.js
+++ script.js
@@ -41,10 +41,12 @@ function calculateIRR() {
 
 function calculateAPandROI() {
     const years = document.getElementById('years').value;
+    const rate = parseFloat(document.getElementById('rate').value) / 100;
     let totalIncome = 0;
     let totalExpenses = 0;
     let cumulativeProfit = 0;
     let paybackPeriod = -1;
+    let npv = 0;
 
     for (let i = 0; i <= fieldCount; i++) {
         const amount = parseFloat(document.getElementById(`income${i}`).value);
@@ -59,13 +61,17 @@ function calculateAPandROI() {
                 }
     }
     const annualProfit = (totalIncome - totalExpenses) / years;
-            const ROI = totalExpenses > 0 ? (annualProfit / totalExpenses) * 100 : 0;
+    const ROI = totalExpenses > 0 ? (annualProfit / totalExpenses) * 100 : 0;
+    const IRR = calculateIRR();     
 
             const messageDiv = document.getElementById('message');
             messageDiv.innerHTML = `Total Income: <span class="income">$${totalIncome.toFixed(2)}</span><br>
                                     Total Expenses: <span class="expense">$${totalExpenses.toFixed(2)}</span><br>
                                     Annual Profit: <span class="income">$${annualProfit.toFixed(2)}</span><br>
                                     ROI: <span class="income">${ROI.toFixed(2)}%</span>;<br>
-                                    Payback Period: <span class="income">${paybackPeriod >= 0 ? paybackPeriod : 'Not achieved'}</span>`;
+                                    Payback Period: <span class="income">${paybackPeriod >= 0 ? paybackPeriod : 'Not achieved'}</span><br>
+                                    NPV: <span class="income">$${npv.toFixed(2)}</span><br>
+                                    IRR: <span class="income">${IRR.toFixed(2)}%</span>`;
+                                   
 
 }    
